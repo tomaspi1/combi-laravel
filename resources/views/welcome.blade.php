d67c090f0e5d7db599cb81386fcb22ad05fadc07
Student <student@primakurzy.cz>
2024-02-01 14:44:48 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,17 @@
     @endif
 
 
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <form action="pridat-chovatele" method="post">
         @csrf
         <label for="jmeno">Jmeno</label>
